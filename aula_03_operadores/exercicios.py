369a4076091f4432a355cc432322ae65ed61690b
ruytdefreitas0601-hue <ruytdefreitas0601@gmail.com>
2026-06-05 17:04:15 -0300

    Create exercicios.py

---
+++ aula_03_operadores/exercicios.py
@@ -0,0 +1,24 @@
+# Aula 03 - Operadores
+# Exercicios
+
+# Exercicio 1: Operadores aritmeticos
+# a = 10
+# b = 3
+# print(a + b)   # adicao
+# print(a - b)   # subtracao
+# print(a * b)   # multiplicacao
+# print(a / b)   # divisao
+# print(a // b)  # divisao inteira
+# print(a % b)   # modulo
+# print(a ** b)  # potencia
+
+# Exercicio 2: Operadores de comparacao
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+
+# Exercicio 3: Operadores logicos
+# print(True and False)
+# print(True or False)
+# print(not True)

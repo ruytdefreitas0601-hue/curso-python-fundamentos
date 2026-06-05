6ce86418a4f6807b7e77fa61761fd06c6376b954
ruytdefreitas0601-hue <ruytdefreitas0601@gmail.com>
2026-06-05 17:07:02 -0300

    Create exercicios.py

---
+++ aula_05_condicionais/exercicios.py
@@ -0,0 +1,31 @@
+# Aula 05 - Condicionais
+# Exercicios
+
+# Exercicio 1: if, elif, else
+# nota = 7
+# if nota >= 9:
+#     print("Excelente!")
+# elif nota >= 7:
+#     print("Bom!")
+# elif nota >= 5:
+#     print("Regular")
+# else:
+#     print("Reprovado")
+
+# Exercicio 2: Numero par ou impar
+# numero = 15
+# if numero % 2 == 0:
+#     print(str(numero) + " e par")
+# else:
+#     print(str(numero) + " e impar")
+
+# Exercicio 3: Condicional aninhada
+# idade = 20
+# tem_carteira = True
+# if idade >= 18:
+#     if tem_carteira:
+#         print("Pode dirigir!")
+#     else:
+#         print("Maior de 18, mas sem carteira")
+# else:
+#     print("Menor de 18 anos")
